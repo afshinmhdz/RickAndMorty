d259afcb685024356142120ed420fee2f0852856
afshinmhdz <afshinmhdz1906@gmail.com>
2024-08-02 10:59:44 +0330

    create Navbar Component ui and added heroicon library

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Navbar from './component/Navbar'
 
 function App() {
  
   return (
-    <>
-    
-    </>
+    <div className="app">
+      <Navbar/>
+    </div>
   )
 }
 
