f0120fd5213a183b6977e9f0cbf089a35af4f7cd
afshinmhdz <afshinmhdz1906@gmail.com>
2024-08-02 14:11:33 +0330

    added CharacterList component (UI) and added data.js (data static)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import CharacterList from './component/CharacterList'
 import Navbar from './component/Navbar'
-
+import {allCharacters} from '../data/data'
 function App() {
  
   return (
     <div className="app">
-      <Navbar/>
+        <Navbar/>  
+      <div className="main">
+        <CharacterList characters={allCharacters}/>
+      </div>
     </div>
   )
 }
