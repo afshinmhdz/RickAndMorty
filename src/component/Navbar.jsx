2674069e8e20a8d4b1058f9c2050f4b970c5f883
afshinmhdz <afshinmhdz1906@gmail.com>
2024-08-04 00:01:24 +0330

    now we have props drilling and to continue we solve it with Component Composition

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,10 @@
 import {HeartIcon} from "@heroicons/react/24/outline"
-function Navbar() {
+function Navbar({nomOfResult}) {
   return (
     <nav className="navbar">
         <div className="navbar__logo">LOGO 😍</div>
         <input type="text" className="text-field" placeholder="Search..." />
-        <div className="navbar__result">found x Character</div>
+        <div className="navbar__result">found {nomOfResult} Character</div>
         <button className="heart">
             <HeartIcon className="icon"/>
             <span className="badge">4</span>
