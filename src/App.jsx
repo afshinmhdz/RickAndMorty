6d5f9d2eadf5c2a18d56f5b41d7c7df11808dc9d
afshinmhdz <afshinmhdz1906@gmail.com>
2024-08-04 00:13:55 +0330

    step 1 of solve props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import './App.css'
-import CharacterList from './component/CharacterList'
-import Navbar from './component/Navbar'
-import CharacterDetail from './component/CharacterDetail'
+import "./App.css";
+import CharacterList from "./component/CharacterList";
+import Navbar from "./component/Navbar";
+import CharacterDetail from "./component/CharacterDetail";
 import { allCharacters } from "../data/data";
-import { useState } from 'react';
+import { useState } from "react";
 function App() {
-  const [characters, setCharacters] = useState(allCharacters)
+  const [characters, setCharacters] = useState(allCharacters);
   return (
     <div className="app">
-        <Navbar nomOfResult={characters.length}/>  
-      <div className="main">
-        <CharacterList  characters={characters}/>
-        <CharacterDetail/>
-      </div>
+      <Navbar nomOfResult={characters.length} />
+      <Main characters={characters}/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+function Main({characters}) {
+  return (
+    <div className="main">
+      <CharacterList characters={characters} />
+      <CharacterDetail />
+    </div>
+  );
+}
